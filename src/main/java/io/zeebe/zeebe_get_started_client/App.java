d5472cf97678adc42e6488e8ecdc409594c47fe9
Avinash Barnwal Home <vnextcoder@gmail.com>
2020-08-07 11:33:55 +0530

    Initial Commit-2

---
+++ src/main/java/io/zeebe/zeebe_get_started_client/App.java
@@ -0,0 +1,100 @@
+package io.zeebe.zeebe_get_started_client;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.api.response.DeploymentEvent;
+import io.zeebe.client.api.response.WorkflowInstanceEvent;
+import io.zeebe.client.api.worker.JobWorker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final ZeebeClient client = ZeebeClient.newClientBuilder()
+				// change the contact point if needed
+				.brokerContactPoint("127.0.0.1:26500").usePlaintext().build();
+
+		System.out.println("Connected.");
+
+		final DeploymentEvent deployment = client.newDeployCommand().addResourceFromClasspath("order-process.bpmn")
+				.send().join();
+
+		final int version = deployment.getWorkflows().get(0).getVersion();
+		System.out.println("Workflow deployed. Version: " + version);
+
+		//final WorkflowInstanceEvent wfInstance = client.newCreateInstanceCommand().bpmnProcessId("order-process")
+			//	.latestVersion().send().join();
+
+		
+
+		
+
+		final Map<String, Object> data = new HashMap<>();
+		data.put("orderId", 31243);
+		data.put("orderItems", Arrays.asList(435, 182, 376));
+
+		final WorkflowInstanceEvent wfInstance = client.newCreateInstanceCommand().bpmnProcessId("Process_Order")
+				.latestVersion().variables(data).send().join();
+
+		final long workflowInstanceKey = wfInstance.getWorkflowInstanceKey();
+		System.out.println("Workflow instance created. Key: " + workflowInstanceKey);
+		// ...
+
+		final JobWorker jobWorker = client.newWorker().jobType("payment-service").handler((jobClient, job) -> {
+			final Map<String, Object> variables = job.getVariablesAsMap();
+
+			System.out.println("Process order: " + variables.get("orderId"));
+			double price = 46.50;
+			System.out.println("Collect money: $" + price);
+
+			// ...
+
+			final Map<String, Object> result = new HashMap<>();
+			result.put("totalPrice", price);
+
+			jobClient.newCompleteCommand(job.getKey()).variables(result).send().join();
+		}).fetchVariables("orderId").open();
+
+		final JobWorker jobWorker2 = client.newWorker().jobType("fetch-service").handler((jobClient, job) -> {
+			final Map<String, Object> variables = job.getVariablesAsMap();
+
+			System.out.println("Process order: " + variables.get("orderId"));
+			double price = 46.50;
+			System.out.println("Collect money: $" + price);
+
+			// ...
+
+			final Map<String, Object> result = new HashMap<>();
+			result.put("totalPrice", price);
+
+			jobClient.newCompleteCommand(job.getKey()).variables(result).send().join();
+		}).fetchVariables("orderId").open();
+
+		final JobWorker jobWorker3 = client.newWorker().jobType("shipping-service").handler((jobClient, job) -> {
+			final Map<String, Object> variables = job.getVariablesAsMap();
+
+			System.out.println("Process order: " + variables.get("orderId"));
+			String shippingReference = "AWB003303";
+			System.out.println("Shipping Reference is : $" + shippingReference);
+
+			// ...
+
+			final Map<String, Object> result = new HashMap<>();
+			result.put("shippingReference", shippingReference);
+
+			jobClient.newCompleteCommand(job.getKey()).variables(result).send().join();
+		}).fetchVariables("shippingReference").open();
+
+		
+		client.close();
+		System.out.println("Closed.");
+
+	}
+}
---
+++ src/test/java/io/zeebe/zeebe_get_started_client/AppTest.java
@@ -0,0 +1,20 @@
+package io.zeebe.zeebe_get_started_client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
